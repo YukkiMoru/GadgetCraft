9c5dc37c04393c5a09061cd005b4022c64f30c7e
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2025-01-10 14:31:43 +0900

    init

---
+++ src/main/kotlin/com/github/YukkiMoru/gadgetCraft/GadgetCraft.kt
@@ -0,0 +1,14 @@
+package com.github.YukkiMoru.gadgetCraft
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class GadgetCraft : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
