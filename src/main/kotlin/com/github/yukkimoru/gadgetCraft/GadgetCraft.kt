a7ebf75a65a9b74de189e100f14e8e1464460099
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2025-01-10 20:02:23 +0900

    add : アイテムの登録

--- src/main/kotlin/com/github/yukkimoru/gadgetCraft/GadgetCraft.kt
+++ src/main/kotlin/com/github/yukkimoru/gadgetCraft/GadgetCraft.kt
@@ -1,13 +1,19 @@
 package com.github.yukkimoru.gadgetCraft
 
 import org.bukkit.plugin.java.JavaPlugin
+import com.github.yukkimoru.gadgetCraft.items.DoubleJumper
+import com.github.yukkimoru.gadgetCraft.items.EnderPack
+import com.github.yukkimoru.gadgetCraft.items.ExplosiveSword
+
 
 class GadgetCraft : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
 
-
+        server.pluginManager.registerEvents(DoubleJumper(this), this)
+//        server.pluginManager.registerEvents(EnderPack(this), this)
+        server.pluginManager.registerEvents(ExplosiveSword(this), this)
     }
 
     override fun onDisable() {
